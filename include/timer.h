939caca9f16c5ada34e935e87f6f5dc9a70e3a56
thanhnv <thanh_bk05@yahoo.com>
2013-06-19 01:45:35 +0700

    thanh: modify timer.h

--- include/timer.h
+++ include/timer.h
@@ -7,7 +7,7 @@
 
 #ifndef __TIMER_H
 #define __TIMER_H
-
+#include "types.h"
 #ifndef TMR_0_REGS
 #define TMR_0_REGS                      (0x01C20000u)
 #endif
@@ -15,55 +15,55 @@
 #define TMR_1_REGS                      (0x01C21000u)
 #endif
 
-#define TIMER_0_REVID					*(unsigned int)(TMR_0_REGS+0x000u)
-#define TIMER_0_EMUMGT					*(unsigned int)(TMR_0_REGS+0x004u)
-#define TIMER_0_GPINTGPEN				*(unsigned int)(TMR_0_REGS+0x008u)
-#define TIMER_0_GPDATGPDIR				*(unsigned int)(TMR_0_REGS+0x00Cu)
-#define TIMER_0_TIM12					*(unsigned int)(TMR_0_REGS+0x010u)
-#define TIMER_0_TIM34					*(unsigned int)(TMR_0_REGS+0x014u)
-#define TIMER_0_PRD12					*(unsigned int)(TMR_0_REGS+0x018u)
-#define TIMER_0_PRD34					*(unsigned int)(TMR_0_REGS+0x01Cu)
-#define TIMER_0_TCR						*(unsigned int)(TMR_0_REGS+0x020u)
-#define TIMER_0_TGCR					*(unsigned int)(TMR_0_REGS+0x024u)
-#define TIMER_0_WDTCR					*(unsigned int)(TMR_0_REGS+0x028u)
-#define TIMER_0_REL12					*(unsigned int)(TMR_0_REGS+0x034u)
-#define TIMER_0_REL34					*(unsigned int)(TMR_0_REGS+0x038u)
-#define TIMER_0_CAP12					*(unsigned int)(TMR_0_REGS+0x03Cu)
-#define TIMER_0_CAP34					*(unsigned int)(TMR_0_REGS+0x040u)
-#define TIMER_0_INTCTLSTAT				*(unsigned int)(TMR_0_REGS+0x044u)
-#define TIMER_0_CMP0					*(unsigned int)(TMR_0_REGS+0x060u)
-#define TIMER_0_CMP1					*(unsigned int)(TMR_0_REGS+0x064u)
-#define TIMER_0_CMP2					*(unsigned int)(TMR_0_REGS+0x068u)
-#define TIMER_0_CMP3					*(unsigned int)(TMR_0_REGS+0x06Cu)
-#define TIMER_0_CMP4					*(unsigned int)(TMR_0_REGS+0x070u)
-#define TIMER_0_CMP5					*(unsigned int)(TMR_0_REGS+0x074u)
-#define TIMER_0_CMP6					*(unsigned int)(TMR_0_REGS+0x078u)
-#define TIMER_0_CMP7					*(unsigned int)(TMR_0_REGS+0x07Cu)
+#define TIMER_0_REVID					*(Uint32)(TMR_0_REGS+0x000u)
+#define TIMER_0_EMUMGT					*(Uint32)(TMR_0_REGS+0x004u)
+#define TIMER_0_GPINTGPEN				*(Uint32)(TMR_0_REGS+0x008u)
+#define TIMER_0_GPDATGPDIR				*(Uint32)(TMR_0_REGS+0x00Cu)
+#define TIMER_0_TIM12					*(Uint32)(TMR_0_REGS+0x010u)
+#define TIMER_0_TIM34					*(Uint32)(TMR_0_REGS+0x014u)
+#define TIMER_0_PRD12					*(Uint32)(TMR_0_REGS+0x018u)
+#define TIMER_0_PRD34					*(Uint32)(TMR_0_REGS+0x01Cu)
+#define TIMER_0_TCR						*(Uint32)(TMR_0_REGS+0x020u)
+#define TIMER_0_TGCR					*(Uint32)(TMR_0_REGS+0x024u)
+#define TIMER_0_WDTCR					*(Uint32)(TMR_0_REGS+0x028u)
+#define TIMER_0_REL12					*(Uint32)(TMR_0_REGS+0x034u)
+#define TIMER_0_REL34					*(Uint32)(TMR_0_REGS+0x038u)
+#define TIMER_0_CAP12					*(Uint32)(TMR_0_REGS+0x03Cu)
+#define TIMER_0_CAP34					*(Uint32)(TMR_0_REGS+0x040u)
+#define TIMER_0_INTCTLSTAT				*(Uint32)(TMR_0_REGS+0x044u)
+#define TIMER_0_CMP0					*(Uint32)(TMR_0_REGS+0x060u)
+#define TIMER_0_CMP1					*(Uint32)(TMR_0_REGS+0x064u)
+#define TIMER_0_CMP2					*(Uint32)(TMR_0_REGS+0x068u)
+#define TIMER_0_CMP3					*(Uint32)(TMR_0_REGS+0x06Cu)
+#define TIMER_0_CMP4					*(Uint32)(TMR_0_REGS+0x070u)
+#define TIMER_0_CMP5					*(Uint32)(TMR_0_REGS+0x074u)
+#define TIMER_0_CMP6					*(Uint32)(TMR_0_REGS+0x078u)
+#define TIMER_0_CMP7					*(Uint32)(TMR_0_REGS+0x07Cu)
 
-#define TIMER_1_REVID					*(unsigned int)(TMR_1_REGS+0x000u)
-#define TIMER_1_EMUMGT					*(unsigned int)(TMR_1_REGS+0x004u)
-#define TIMER_1_GPINTGPEN				*(unsigned int)(TMR_1_REGS+0x008u)
-#define TIMER_1_GPDATGPDIR				*(unsigned int)(TMR_1_REGS+0x00Cu)
-#define TIMER_1_TIM12					*(unsigned int)(TMR_1_REGS+0x010u)
-#define TIMER_1_TIM34					*(unsigned int)(TMR_1_REGS+0x014u)
-#define TIMER_1_PRD12					*(unsigned int)(TMR_1_REGS+0x018u)
-#define TIMER_1_PRD34					*(unsigned int)(TMR_1_REGS+0x01Cu)
-#define TIMER_1_TCR						*(unsigned int)(TMR_1_REGS+0x020u)
-#define TIMER_1_TGCR					*(unsigned int)(TMR_1_REGS+0x024u)
-#define TIMER_1_WDTCR					*(unsigned int)(TMR_1_REGS+0x028u)
-#define TIMER_1_REL12					*(unsigned int)(TMR_1_REGS+0x034u)
-#define TIMER_1_REL34					*(unsigned int)(TMR_1_REGS+0x038u)
-#define TIMER_1_CAP12					*(unsigned int)(TMR_1_REGS+0x03Cu)
-#define TIMER_1_CAP34					*(unsigned int)(TMR_1_REGS+0x040u)
-#define TIMER_1_INTCTLSTAT				*(unsigned int)(TMR_1_REGS+0x044u)
-#define TIMER_1_CMP0					*(unsigned int)(TMR_1_REGS+0x060u)
-#define TIMER_1_CMP1					*(unsigned int)(TMR_1_REGS+0x064u)
-#define TIMER_1_CMP2					*(unsigned int)(TMR_1_REGS+0x068u)
-#define TIMER_1_CMP3					*(unsigned int)(TMR_1_REGS+0x06Cu)
-#define TIMER_1_CMP4					*(unsigned int)(TMR_1_REGS+0x070u)
-#define TIMER_1_CMP5					*(unsigned int)(TMR_1_REGS+0x074u)
-#define TIMER_1_CMP6					*(unsigned int)(TMR_1_REGS+0x078u)
-#define TIMER_1_CMP7					*(unsigned int)(TMR_1_REGS+0x07Cu)
+#define TIMER_1_REVID					*(Uint32)(TMR_1_REGS+0x000u)
+#define TIMER_1_EMUMGT					*(Uint32)(TMR_1_REGS+0x004u)
+#define TIMER_1_GPINTGPEN				*(Uint32)(TMR_1_REGS+0x008u)
+#define TIMER_1_GPDATGPDIR				*(Uint32)(TMR_1_REGS+0x00Cu)
+#define TIMER_1_TIM12					*(Uint32)(TMR_1_REGS+0x010u)
+#define TIMER_1_TIM34					*(Uint32)(TMR_1_REGS+0x014u)
+#define TIMER_1_PRD12					*(Uint32)(TMR_1_REGS+0x018u)
+#define TIMER_1_PRD34					*(Uint32)(TMR_1_REGS+0x01Cu)
+#define TIMER_1_TCR						*(Uint32)(TMR_1_REGS+0x020u)
+#define TIMER_1_TGCR					*(Uint32)(TMR_1_REGS+0x024u)
+#define TIMER_1_WDTCR					*(Uint32)(TMR_1_REGS+0x028u)
+#define TIMER_1_REL12					*(Uint32)(TMR_1_REGS+0x034u)
+#define TIMER_1_REL34					*(Uint32)(TMR_1_REGS+0x038u)
+#define TIMER_1_CAP12					*(Uint32)(TMR_1_REGS+0x03Cu)
+#define TIMER_1_CAP34					*(Uint32)(TMR_1_REGS+0x040u)
+#define TIMER_1_INTCTLSTAT				*(Uint32)(TMR_1_REGS+0x044u)
+#define TIMER_1_CMP0					*(Uint32)(TMR_1_REGS+0x060u)
+#define TIMER_1_CMP1					*(Uint32)(TMR_1_REGS+0x064u)
+#define TIMER_1_CMP2					*(Uint32)(TMR_1_REGS+0x068u)
+#define TIMER_1_CMP3					*(Uint32)(TMR_1_REGS+0x06Cu)
+#define TIMER_1_CMP4					*(Uint32)(TMR_1_REGS+0x070u)
+#define TIMER_1_CMP5					*(Uint32)(TMR_1_REGS+0x074u)
+#define TIMER_1_CMP6					*(Uint32)(TMR_1_REGS+0x078u)
+#define TIMER_1_CMP7					*(Uint32)(TMR_1_REGS+0x07Cu)
 
 typedef struct Timer_t{
 	 Int32 (*callback)(void * data);
@@ -79,7 +79,7 @@ typedef struct Timer_t{
 /**************************************************************************\
 * Overlay structure typedef definition
 \**************************************************************************/
-typedef volatile TmrRegs         *TmrRegsOvly;
+
 
 /**************************************************************************\
 * Field Definition Macros
@@ -706,5 +706,161 @@ typedef volatile TmrRegs         *TmrRegsOvly;
 #define TMR_CMP7_CMP7_RESETVAL       (0xFFFFFFFFu)
 
 #define TMR_CMP7_RESETVAL            (0x00000000u)
+
+/*
+** Values that can be passed to Timer APIs as timer
+*/
+#define TMR_TIMER12                        (0x00003FFEu) /* Timer12 */
+#define TMR_TIMER34                        (0x3FFE0000u) /* Timer34 */
+#define TMR_TIMER_BOTH                     (0x3FFE3FFEu) /* Both Timers */
+/*****************************************************************************/
+/*
+** Values that can be passed to TimerEnable API as enaMode to select the mode
+*/
+/* Enable the Timer for once */
+#define TMR_ENABLE_ONCE                    (0x00400040u)
+
+/* Enable the Timer for continuous operation without period reload */
+#define TMR_ENABLE_CONT                    (0x00800080u)
+
+/* Enable the Timer for continuous operation with period reload */
+#define TMR_ENABLE_CONTRELOAD              (0x00C000C0u)
+
+/*****************************************************************************/
+/*
+** Values that can be passed to TimerConfigure API as config
+*/
+/* 64 Bit mode with internal clock source */
+#define TMR_CFG_64BIT_CLK_INT              (0x00000013u)
+
+/* 64 Bit mode with external clock source */
+#define TMR_CFG_64BIT_CLK_EXT              (0x01000113u)
+
+/* 64 Bit Watchdog timer mode */
+#define TMR_CFG_64BIT_WATCHDOG             (0x0000001Bu)
+
+/* 32 Bit chained mode with internal clock source */
+#define TMR_CFG_32BIT_CH_CLK_INT           (0x0000001Fu)
+
+/* 32 Bit chained mode with external clock source */
+#define TMR_CFG_32BIT_CH_CLK_EXT           (0x0100011Fu)
+
+/* 32 Bit unchained mode with internal clock source for both timers */
+#define TMR_CFG_32BIT_UNCH_CLK_BOTH_INT    (0x00000017u)
+
+/* 32 Bit unchained mode; clock Internal for Timer12; external for Timer34 */
+#define TMR_CFG_32BIT_UNCH_CLK_12INT_34EXT (0x01000017u)
+
+/* 32 Bit unchained mode; clock external for Timer12; internal for Timer34 */
+#define TMR_CFG_32BIT_UNCH_CLK_12EXT_34INT (0x00000117u)
+
+/* 32 Bit unchained mode with external clock source for both timers */
+#define TMR_CFG_32BIT_UNCH_CLK_BOTH_EXT    (0x01000117u)
+/*****************************************************************************/
+/*
+** Values that can be passed to TimerIntEnable/IntDisable APIs as intFlag
+** Any combination is also allowed.
+** Example- (TMR_INTCTLSTAT_PRDINTEN12 | TMR_INTCTLSTAT_EVTINTEN34)
+*/
+/* To enable/disable the interrupt for Timer12 in Capture mode */
+#define TMR_INT_TMR12_CAPT_MODE            (TMR_INTCTLSTAT_EVTINTEN12)
+
+/* To enable/disable the interrupt for Timer12 in in 64 Bit/32 Bit/WDT mode */
+#define TMR_INT_TMR12_NON_CAPT_MODE        (TMR_INTCTLSTAT_PRDINTEN12)
+
+/* To enable/disable the interrupt for Timer34 in Capture mode */
+#define TMR_INT_TMR34_CAPT_MODE            (TMR_INTCTLSTAT_EVTINTEN34)
+
+/* To enable/disable the interrupt for Timer34 in 64 Bit/32 Bit/WDT mode */
+#define TMR_INT_TMR34_NON_CAPT_MODE        (TMR_INTCTLSTAT_PRDINTEN34)
+
+/*****************************************************************************/
+/*
+** Values that can be passed to TimerIntStatusGet/Clear as statFlag
+** Any combination is also allowed.
+*/
+/* Interrupt Generated when Timer 12 in 64 Bit/32 Bit/WDT mode */
+#define TMR_INTSTAT12_TIMER_NON_CAPT       (TMR_INTCTLSTAT_PRDINTSTAT12)
+
+/* Interrupt Generated when Timer 12 in capture mode */
+#define TMR_INTSTAT12_TIMER_CAPT           (TMR_INTCTLSTAT_EVTINTSTAT12)
+
+/* Interrupt Generated when Timer 34 in 64 Bit/32 Bit/WDT mode */
+#define TMR_INTSTAT34_TIMER_NON_CAPT       (TMR_INTCTLSTAT_PRDINTSTAT34)
+
+/* Interrupt Generated when Timer 34 in capture mode */
+#define TMR_INTSTAT34_TIMER_CAPT           (TMR_INTCTLSTAT_EVTINTSTAT12)
+
+/*****************************************************************************/
+/*
+** Values that can be passed to TimerCaptureConfig as cfgCap
+*/
+/* Disable Capture Mode */
+#define TMR_CAPT_DISABLE                   (0x00000000)
+
+/* Enable Capture Mode. Event Occurs in rising edge */
+#define TMR_CAPT_ENABLE_RIS_EDGE           (0x08000800)
+
+/* Enable Capture Mode. Event Occurs in falling edge */
+#define TMR_CAPT_ENABLE_FALL_EDGE          (0x18001800)
+
+/* Enable Capture Mode. Event Occurs in both edges */
+#define TMR_CAPT_ENABLE_BOTH_EDGE          (0x28002800)
+
+/*****************************************************************************/
+/*
+** Values that can be passed to TimerPulseWidthSet as pulseWidth
+*/
+#define TMR_PULSE_WIDTH_1_CLK              (0x00000000) /* 1 clock cycle */
+#define TMR_PULSE_WIDTH_2_CLK              (0x00100010) /* 2 clock cycles */
+#define TMR_PULSE_WIDTH_3_CLK              (0x00200020) /* 3 clock cycles */
+#define TMR_PULSE_WIDTH_4_CLK              (0x00300030) /* 4 clock cycles */
+
+/*****************************************************************************/
+/*
+** Values returned by TimerOUTStatusGet API
+*/
+#define TMR_OUT12_ASSERTED                 (TMR_TCR_TSTAT12) /* TMR64P_OUT12 */
+#define TMR_OUT34_ASSERTED                 (TMR_TCR_TSTAT34) /* TMR64P_OUT34 */
+
+/*
+** Prototypes for the APIs
+*/
+extern Uint32 TimerCompareGet(Uint32 module,Uint32 regIndex);
+extern Uint32 TimerOUTStatusGet(Uint32 module,Uint32 timer);
+extern Uint32 TimerIntStatusGet(Uint32 module,Uint32 statFlag);
+extern Uint32 TimerIntStatusClear(Uint32 module,Uint32 statFlag);
+extern Uint32 TimerCaptureGet(Uint32 module,Uint32 timer);
+extern Uint32 TimerCounterGet(Uint32 module,Uint32 timer);
+extern Uint32 TimerPeriodGet(Uint32 module,Uint32 timer);
+extern Uint32 TimerReloadGet(Uint32 module,Uint32 timer);
+extern Uint32 TimerPreScalarCount34Get(Uint32 module);
+extern Uint32 TimerDivDwnRatio34Get(Uint32 module);
+
+extern void TimerCounterSet(Uint32 module,Uint32 timer,Uint32 counter);
+extern void TimerPeriodSet(Uint32 module,Uint32 timer,Uint32 period);
+extern void TimerReloadSet(Uint32 module,Uint32 timer,Uint32 reload);
+extern void TimerEnable(Uint32 module,Uint32 timer,Uint32 enaMode);
+extern void TimerPreScalarCount34Set(Uint32 module,Uint32 psc34);
+extern void TimerDivDwnRatio34Set(Uint32 module,Uint32 tddr34);
+extern void TimerInvertOUTDisable(Uint32 module,Uint32 timer);
+extern void TimerReadResetDisable(Uint32 module,Uint32 timer);
+extern void TimerInputGateDisable(Uint32 module,Uint32 timer);
+extern void TimerReadResetEnable(Uint32 module,Uint32 timer);
+extern void TimerInvertINDisable(Uint32 module,Uint32 timer);
+extern void TimerInvertOUTEnable(Uint32 module,Uint32 timer);
+extern void TimerInputGateEnable(Uint32 module,Uint32 timer);
+extern void TimerInvertINEnable(Uint32 module,Uint32 timer);
+extern void TimerIntDisable(Uint32 module,Uint32 intFlags);
+extern void TimerClockModeSet(Uint32 module,Uint32 timer);
+extern void TimerPulseModeSet(Uint32 module,Uint32 timer);
+extern void TimerIntEnable(Uint32 module,Uint32 intFlags);
+extern void TimerConfigure(Uint32 module,Uint32 config);
+extern void TimerDisable(Uint32 module,Uint32 timer);
+extern void TimerWatchdogReactivate(Uint32 module,Uint32 timer);
+extern void TimerWatchdogActivate(Uint32 module,Uint32 timer);
+extern void TimerCaptureConfigure(Uint32 module,Uint32 timer,Uint32 cfgCap);
+extern void TimerCompareSet(Uint32 module,Uint32 regIndex,Uint32 compare);
+extern void TimerPulseWidthSet(Uint32 module,Uint32 timer,Uint32 pulseWidth);
 
 #endif
